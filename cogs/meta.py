ad65d14b421a84fda0193dbfb107788ff1c2d15a
和泉田済人 <sumito@izumita.com>
2019-09-17 19:15:56 +0900

    infoコマンド追加

--- cogs/meta.py
+++ cogs/meta.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from .utils import colours, checks
 import discord
+import pkg_resources
 from cogs.utils.database import *
 
 
@@ -18,6 +19,48 @@ class Meta(commands.Cog):
         embed = discord.Embed(title=f'{ctx.author.name}さんの情報',
                               description=f'登録Webhook数: {len(webhook)}\nサブスクリプションの有無: {is_subsc}',
                               color=colours.deepskyblue)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def info(self, ctx):
+        """Botの詳細な情報を表示します。"""
+        embed = discord.Embed(title=self.bot.user.name, color=colours.deepskyblue)
+        owner = self.bot.get_user(self.bot.owner_id)
+        embed.set_author(name=str(owner), icon_url=owner.avatar_url)
+
+        total_members = 0
+        total_online = 0
+        offline = discord.Status.offline
+        for member in self.bot.get_all_members():
+            total_members += 1
+            if member.status is not offline:
+                total_online += 1
+
+        total_unique = len(self.bot.users)
+
+        text = 0
+        voice = 0
+        guilds = 0
+        for guild in self.bot.guilds:
+            guilds += 1
+            for channel in guild.channels:
+                if isinstance(channel, discord.TextChannel):
+                    text += 1
+                elif isinstance(channel, discord.VoiceChannel):
+                    voice += 1
+
+        embed.add_field(name='Members',
+                        value=f'{total_members} total\n{total_unique} unique\n{total_online} unique online')
+        embed.add_field(name='Channels', value=f'{text + voice} total\n{text} text\n{voice} voice')
+
+        version = pkg_resources.get_distribution('discord.py').version
+        embed.add_field(name='Guilds', value=str(guilds))
+
+        embed.add_field(name='登録ユーザー', value=f'{len(await Auth.query.gino.all())}')
+        embed.add_field(name='登録webhook', value=f'{len(await Webhook.query.gino.all())}')
+
+        embed.set_footer(text=f'discord.py v{version}', icon_url='http://i.imgur.com/5BFecvA.png')
+
         await ctx.send(embed=embed)
 
 
