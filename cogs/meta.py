d018263db6b8617667bcc9958cb60152a574732a
和泉田済人 <sumito@izumita.com>
2019-09-17 20:01:01 +0900

    error fix

--- cogs/meta.py
+++ cogs/meta.py
@@ -14,7 +14,7 @@ class Meta(commands.Cog):
     async def dashboard(self, ctx):
         """登録されているWebhookなどの現在の状況を表示します。"""
         webhook = await Webhook.query.where(Webhook.discord_user_id == str(ctx.author.id)).gino.all()
-        subsc = Subscription.query.where(Subscription.id == str(ctx.author.id)).gino.first()
+        subsc = await Subscription.query.where(Subscription.id == str(ctx.author.id)).gino.first()
         is_subsc = 'はい' if subsc else 'いいえ'
         embed = discord.Embed(title=f'{ctx.author.name}さんの情報',
                               description=f'登録Webhook数: {len(webhook)}\nサブスクリプションの有無: {is_subsc}',
